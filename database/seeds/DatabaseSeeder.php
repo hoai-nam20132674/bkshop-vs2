88bef1487a7fda6a5991a8395d5e0169d4bb65f0
hoai-nam20132674 <namnguyen20132674@gmail.com>
2019-04-24 18:13:30 +0700

    1

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(UsersClientTableSeeder::class);
-        // $this->call(SystemsTableSeeder::class);
-        // $this->call(PropertiesTypeTableSeeder::class);
+        $this->call(SystemsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(UsersClientTableSeeder::class);
+        $this->call(PropertiesTypeTableSeeder::class);
         $this->call(PropertiesTableSeeder::class);
     }
 }
